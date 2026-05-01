38580f91e10a31b6ad03d2147d93de04ba511132
Raihan <raihanmohd10@gmail.com>
2026-05-01 19:45:31 +0530

    Add files via upload

---
+++ java/com/jay/role_based_authentication/RoleBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.role_based_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
